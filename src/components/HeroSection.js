9be995e8d4ae1868c15aab990a3a50605ad6b202
Archil <archilp@umich.edu>
2021-03-30 11:18:39 -0400

    firstcommit

---
+++ src/components/HeroSection.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Button } from "./Button";
+import "../App.css";
+import "./HeroSection.css";
+
+import Video from "./back2.mp4";
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      {/* <video src={video} type="video/mp4" autoPlay={true} loop muted /> */}
+
+      <video src={Video} type="video/mp4" autoPlay={true} loop muted />
+
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          ButtonSize="btn--large"
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          ButtonSize="btn--large"
+        >
+          Watch Trailer <i className="far fa-play-circle" />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
